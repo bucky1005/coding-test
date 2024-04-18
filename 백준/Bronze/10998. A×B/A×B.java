b190d75736ddbab9fad7ab19e6b82065a4898ee2
bucky1005 <87284220+bucky1005@users.noreply.github.com>
2024-04-18 16:51:22 +0900

    [Bronze V] Title: A×B, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/10998. A×B/A×B.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+
+        System.out.println(multiTwoNumbers(a,b));
+    }
+
+    private static int multiTwoNumbers(int a, int b) {
+        return a * b;
+    }
+}
