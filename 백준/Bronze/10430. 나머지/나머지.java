1e234c69d7da6a8788ec55ef793bb7b39a351b30
bucky1005 <87284220+bucky1005@users.noreply.github.com>
2024-04-18 16:50:42 +0900

    [Bronze V] Title: 나머지, Time: 204 ms, Memory: 17668 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int a = 0, b = 0, c = 0, input;
+        
+        for (int i = 0; i < 3; i++) {
+            input = sc.nextInt();
+            
+            if(i == 0 && input >= 2)
+                a = input;
+            
+            if(i == 1 && input <= 10000)
+                b = input;
+                
+            if(i == 2 && input <= 10000)
+                c = input;
+        }
+
+        System.out.println((a + b) % c);
+        System.out.println(((a % c) + (b % c)) % c);
+        System.out.println((a * b) % c);
+        System.out.println(((a % c) * (b % c)) % c);
+		}
+}
